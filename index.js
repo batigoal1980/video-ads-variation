8b48c904e5d4f1a111c644b1c114fbf8699960dc
batigoal1980 <dongyu6@yahoo.com>
2024-10-01 09:34:52 -0700

    Initial commit for new deployment

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-    const express = require('express');
-    const app = express();
-    const port = process.env.PORT || 3000;
+// index.js
 
-    app.get('/', (req, res) => res.send('Hello World!'));
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-    app.listen(port, () => console.log(`App listening on port ${port}`));
+// Middleware to serve static files from the React app
+app.use(express.static(path.join(__dirname, '../video-ads-uploader/build')));
+
+// API routes can be defined here
+// Example: app.use('/api', require('./routes/api'));
+
+// Catch-all handler to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../video-ads-uploader/build/index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
